c5cc17dd27f095194af95e786ba8b16b8cb819fe
Alexis La Porte <aklaporte@gmail.com>
2018-12-27 02:03:14 -0500

    Add 12-4 Key Game exercise.

---
+++ ch-12/key_game/key_game.py
@@ -0,0 +1,27 @@
+import pygame
+import sys
+
+# Initialize a pygame instance.
+def run_game():
+    pygame.init()
+    # Settings
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption('Key Game')
+    bg_color = (230, 230, 230)
+    screen.fill(bg_color)
+    font = pygame.font.SysFont('Arial', 65, True)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                # Get key as an integer, pass to key.name() to return human readable name
+                key_name = pygame.key.name(event.key)
+                text_surface = font.render(key_name, True, (0, 0, 0), bg_color)
+
+                screen.fill(bg_color)  # Fill the window with a bg color
+                screen.blit(text_surface, (0, 0))  # Draw text on window (text surface on window surface)
+                pygame.display.flip()  # Draw the latest window
+
+run_game()
