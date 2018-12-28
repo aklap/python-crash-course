b95594cffcf67474e747459cb82f24c360a12d83
Alexis La Porte <aklaporte@gmail.com>
2018-12-27 19:03:22 -0500

    Rocket Game: Add events checking loop.

--- ch-12/rocket_game/run_game.py
+++ ch-12/rocket_game/run_game.py
@@ -5,3 +5,8 @@ def run_game():
     pygame.init()
     pygame.display.mode(1200, 800)
     pygame.display.set_caption('Rocket Game')
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
