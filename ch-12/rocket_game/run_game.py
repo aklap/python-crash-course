b56250b245829b65cf04f72428d6913b63534e27
Alexis La Porte <aklaporte@gmail.com>
2018-12-27 19:09:28 -0500

    Rocket Game: Add background color.
    - Fixes set_mode arguments.
    - Changes color of window background to black.

--- ch-12/rocket_game/run_game.py
+++ ch-12/rocket_game/run_game.py
@@ -3,14 +3,16 @@ import pygame
 
 def run_game():
     pygame.init()
-    pygame.display.mode(1200, 800)
+    screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption('Rocket Game')
+    bg_color = (0, 0, 0)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        screen.fill(bg_color)
         pygame.display.flip()
 
 run_game()
