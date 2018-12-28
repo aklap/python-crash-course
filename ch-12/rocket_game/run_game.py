7304ea3ab5543fab7203b56ccb4ff16389175abf
Alexis La Porte <aklaporte@gmail.com>
2018-12-27 19:22:54 -0500

    Rocket Game: Add Rocket class.

--- ch-12/rocket_game/run_game.py
+++ ch-12/rocket_game/run_game.py
@@ -2,12 +2,30 @@ import sys
 import pygame
 from settings import Settings
 
+class Rocket():
+    """Base rocket class."""
+    def __init__(self, settings, screen):
+        """Initialize rocket."""
+        self.screen = screen
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        """Draw rocket at current location."""
+        self.screen.blit(self.image, self.rect)
+
+
 def run_game():
     pygame.init()
     settings = Settings()
     screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
     pygame.display.set_caption('Rocket Game')
     bg_color = (0, 0, 0)
+    rocket = Rocket(settings, screen)
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +33,7 @@ def run_game():
                 sys.exit()
 
         screen.fill(settings.bg_color)
+        rocket.blitme()
         pygame.display.flip()
 
 run_game()
