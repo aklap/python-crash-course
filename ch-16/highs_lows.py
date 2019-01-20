18d2c754c26f88d581f806a36c819a9af263f4e0
Alexis La Porte <aklaporte@gmail.com>
2019-01-19 23:27:55 -0500

    Plot high, low temps with dates.

--- ch-16/highs_lows.py
+++ ch-16/highs_lows.py
@@ -1,17 +1,39 @@
 import csv
+from datetime import datetime
 
-filename = 'sitka_weather_07-2014.csv'
+from matplotlib import pyplot as plt
+
+
+filename = 'sitka_weather_2014.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)  # Store this as CSV reader object
     header_row = next(reader)  # Call next once, to get list of headers
-    # print(header_row)  # Print the headers
 
-    # Note: Use enumerate to acces the index
-    # for index, column_header in enumerate(header_row):
-    #     print(index, column_header)
+    # FIXME: Why can't I use multiple list comprehensions? Only the first returns
+    # highs = [int(row[1]) for row in reader]
+    # dates = [datetime.strptime(row[0], "%Y-%m-%d") for row in reader]
+    highs, dates, lows = [], [], []
+
+    for row in reader:
+        highs.append(int(row[1]))
+        dates.append(datetime.strptime(row[0], "%Y-%m-%d"))
+        lows.append(int(row[3]))
     
-    # Get high temperature: Store the first value of each row
-    highs = [int(row[1]) for row in reader]
+    # Plot data
+    fig = plt.figure(dpi=128, figsize=(10,6))
+    plt.plot(dates, highs, c='red', alpha=0.5)
+    plt.plot(dates, lows, c='blue', alpha=0.5)
 
-    print(highs)
+    # Format plot:
+    # Title
+    plt.title("Daily high temperatures, July 2014", fontsize=24)
+    # X axis label
+    plt.xlabel("", fontsize=16)
+    fig.autofmt_xdate()
+    # Y axis label
+    plt.ylabel("Temperature (F)", fontsize=16)
+    # Place ticks on both axes
+    plt.tick_params(axis='both', which='major', labelsize=16)
+    # Show the graph
+    plt.show()
