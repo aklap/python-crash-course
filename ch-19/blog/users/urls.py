6a2d1cbc18a003a097dd87cff58764562f617e17
Alexis La Porte <aklaporte@gmail.com>
2019-01-24 17:57:14 -0500

    Add logout feature.

--- ch-19/blog/users/urls.py
+++ ch-19/blog/users/urls.py
@@ -7,4 +7,5 @@ app_name = 'users'
 urlpatterns = [
     # Login page
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
